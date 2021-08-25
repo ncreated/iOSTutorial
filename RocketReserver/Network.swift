384873bf48f4d0b29cb54812282373829896fa79
Maciek Grzybowski <maciek.grzybowski@datadoghq.com>
2021-08-25 14:12:31 +0200

    Add `dd-sdk-ios` instrumentation

--- RocketReserver/Network.swift
+++ RocketReserver/Network.swift
@@ -9,12 +9,32 @@
 import Foundation
 import Apollo
 import KeychainSwift
+import Datadog
+
+internal class MyURLSessionClient: URLSessionClient, __URLSessionDelegateProviding {
+    let ddURLSessionDelegate = DDURLSessionDelegate()
+
+    override func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
+        ddURLSessionDelegate.urlSession(session, task: task, didFinishCollecting: metrics)
+        super.urlSession(session, task: task, didFinishCollecting: metrics)
+    }
+
+    override func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        ddURLSessionDelegate.urlSession(session, task: task, didCompleteWithError: error)
+        super.urlSession(session, task: task, didCompleteWithError: error)
+    }
+
+    override func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        ddURLSessionDelegate.urlSession(session, dataTask: dataTask, didReceive: data)
+        super.urlSession(session, dataTask: dataTask, didReceive: data)
+    }
+}
 
 class Network {
     static let shared = Network()
     
     private(set) lazy var apollo: ApolloClient = {
-        let client = URLSessionClient()
+        let client = MyURLSessionClient()
         let cache = InMemoryNormalizedCache()
         let store = ApolloStore(cache: cache)
         let provider = NetworkInterceptorProvider(client: client, store: store)
